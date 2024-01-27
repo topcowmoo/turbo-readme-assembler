0aeb85b01a88259d461c979f52b593e5a2510de6
Salvatore <mammoliti.sam@gmail.com>
2024-01-27 14:29:24 -0500

    "added packages needed to run the appliacation"

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Create an array of questions for user input
+const questions = [
+
+
+
+
+
+    
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
