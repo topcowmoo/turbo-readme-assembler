ba23e5188ed42f8f29bfbe0db7b72c5bee19b6bd
Salvatore <mammoliti.sam@gmail.com>
2024-01-27 15:03:38 -0500

    "Create an array of questions for user input in index.js"

--- index.js
+++ index.js
@@ -4,13 +4,66 @@ const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 // Create an array of questions for user input
-const questions = [
-
-
-
-
-
-    
+const questions = [{
+        type: "input",
+        message: "What is the title of your README project?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "What is the motivation of this project?",
+        name: "motivation"
+    },
+    {   
+        type: "input",
+        message: "Why did you build this project?",
+        name: "build"
+    },
+    {
+        type: "input",
+        message: "What problem does it solve or help with?",
+        name: "problem"
+    },
+    {
+        type: "input",
+        message: "What did you learn building this project?",
+        name: "learn"
+    },
+    {
+        type: "input",
+        message: "What is the motivation of this project?",
+        name: "motivation"
+    },
+    {
+        type: "input",
+        message: "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "Provide instructions and examples for use of the project. Include screenshots as needed",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "List your collaborators, if any, with links to their GitHub profiles",
+        name: "collaborators"
+    },
+    {
+        type: "input",
+        message: "The last section of a README file is the licence. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/)",
+        name: "licence"
+    },
+    {
+        type: "input",
+        message: "Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/)",
+        name: "badges"
+    },
+    {
+        type: "input",
+        message: "If your project has a lot of features, list them here",
+        name: "features"
+    },
 ];
 
 // TODO: Create a function to write README file
