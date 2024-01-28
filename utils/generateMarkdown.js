be8067204d3ac99c0ffb277bb64acf743c29f124
Salvatore <mammoliti.sam@gmail.com>
2024-01-28 14:17:04 -0500

    "troubleshooting error regarding badges not rendering at top of page"

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,122 +1,87 @@
-// Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  if (!license) {
-    return '';
-  }
+const fs = require('fs');
+const inquirer = require('inquirer');
 
-  switch (license.toLowerCase()) {
-    case 'apache license 2.0':
-      return '[![License: Apache-2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
-    case 'microsoft public license':
-      return '[![License: MS-PL](https://img.shields.io/badge/License-MS--PL-green.svg)](https://opensource.org/licenses/MS-PL)';
-    case 'mit':
-      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
-    case 'mozilla public license 2.0':
-      return '[![License: MPL-2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
-    default:
-      return '';
-  }
-}
-
-// Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {
-  if (!license) {
-    return '';
-  }
-
-  switch (license.toLowerCase()) {
-    case 'apache license 2.0':
-      return '[Apache License 2.0](https://opensource.org/licenses/Apache-2.0)';
-    case 'microsoft public license':
-      return '[Microsoft Public License](https://opensource.org/licenses/MS-PL)';
-    case 'mit':
-      return '[MIT License](https://opensource.org/licenses/MIT)';
-    case 'mozilla public license 2.0':
-      return '[Mozilla Public License 2.0](https://opensource.org/licenses/MPL-2.0)';
-    default:
-      return '';
-  }
-}
-
-// Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-  if (!license) {
-    return '';
-  }
-
-  switch (license.toLowerCase()) {
-    case 'apache license 2.0':
-      return `
-## License
-
-This project is licensed under the [Apache License 2.0](https://opensource.org/licenses/Apache-2.0).
-`;
-    case 'microsoft public license':
-      return `
-## License
-
-This project is licensed under the [Microsoft Public License](https://opensource.org/licenses/MS-PL).
-`;
-    case 'mit':
-      return `
-## License
-
-This project is licensed under the [MIT License](https://opensource.org/licenses/MIT).
-`;
-    case 'mozilla public license 2.0':
-      return `
-## License
-
-This project is licensed under the [Mozilla Public License 2.0](https://opensource.org/licenses/MPL-2.0).
-`;
-    default:
-      return '';
-  }
-}
+// ... (Your existing code for renderLicenseBadge, renderLicenseLink, renderLicenseSection)
 
 // Create a function to generate markdown for README
 function generateMarkdown(data) {
+  const licenseBadge = renderLicenseBadge(data.license);
+  const licenseSection = renderLicenseSection(data.license);
+  
   return `# ${data.title}
-    ![Github license] (http://img.shields.io/badge/license-${data.license}-blue.svg)
+  
+${licenseBadge}
 
-    ## Description
-    ${data.motivation}
-    ${data.build}
-    ${data.problem}
-    ${data.learn}
+## Description
+${data.motivation}
+${data.build}
+${data.problem}
+${data.learn}
 
-    ## Table of Contents
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [License](#license)
-    - [Contributing](#contributing)
-    - [Tests](#tests)
-    - [Questions](#questions)
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
 
-    ## Installation
-    ${data.installation}
+## Installation
+${data.installation}
 
-    ## Usage
-    ${data.usage}
+## Usage
+${data.usage}
 
-    ## License
-    ${data.license}
+## License
+${licenseSection}
 
-    ## Contributing
-    ${data.contributing}
+## Contributing
+${data.contributing}
 
-    ## Tests
-    ${data.test}
+## Tests
+${data.test}
     
-    ## Questions
+## Questions
     
-    If you have any questions about this projects, please contact me directly at ${data.email}. My GitHub profile can be found at https://github.com/${data.github}.
+If you have any questions about this projects, please contact me directly at ${data.email}. My GitHub profile can be found at https://github.com/${data.github}.
 
-    ---
-    `;
+---
+`;
 }
 
-module.exports = generateMarkdown;
+// ... (Your existing code)
+
+// Create a function to write README file
+function writeToFile(data) {
+    const folderPath = 'sample';  // specify the folder name
+    const fileName = 'README-sample.md';
+    const filePath = `${folderPath}/${fileName}`;
+
+    // Check if the folder exists, create it if not
+    if (!fs.existsSync(folderPath)) {
+        fs.mkdirSync(folderPath);
+    }
+
+    fs.writeFile(filePath, data, (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log(`README file created successfully at ${filePath}`);
+        }
+    });
+}
+
+// Create a function to initialize app
+function init() {
+    inquirer.prompt(questions)
+    .then((answers) => {
+        const readmeContent = generateMarkdown(answers);
+        writeToFile(readmeContent);
+    })
+    .catch((error) => {
+        console.error("An error occurred during initialization:", error);
+    });
+}
+
+// Function call to initialize app
+init();
