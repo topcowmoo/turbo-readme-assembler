f4387affccac216ff665eb8b8fb97c70d0e86d0d
Salvatore <mammoliti.sam@gmail.com>
2024-01-28 14:26:04 -0500

    "added questions variable to line 4"

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,68 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-// ... (Your existing code for renderLicenseBadge, renderLicenseLink, renderLicenseSection)
+const questions = [{
+    type: "input",
+        message: "What is the title of your README project?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "What is the motivation of this project?",
+        name: "motivation"
+    },
+    {   
+        type: "input",
+        message: "Why did you build this project?",
+        name: "build"
+    },
+    {
+        type: "input",
+        message: "What problem does it solve or help with?",
+        name: "problem"
+    },
+    {
+        type: "input",
+        message: "What did you learn building this project?",
+        name: "learn"
+    },
+    {
+        type: "input",
+        message: "What are the steps required to install your project?:",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "Provide instructions and examples for use of the project:",
+        name: "usage"
+    },
+    {
+        type: "checkbox",
+        message: "Select a license for your project:",
+        name: "license",
+        choices: ['Apache license 2.0', 'Microsoft Public License', 'MIT', 'Mozilla Public License 2.0']
+    },
+    {
+        type: "input",
+        message: "List your collaborators, if any, with links to their GitHub profiles:",
+        name: "contributing"
+    },
+    {
+        type: "input",
+        message: "Please enter your github username:",
+        name: "github"
+    },
+    {
+        type: "input",
+        message: "Please enter your email address:",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What command should be run for tests>",
+        name: "test"
+    }
+];
 
 // Create a function to generate markdown for README
 function generateMarkdown(data) {
